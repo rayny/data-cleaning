9934936ed55460ab577684365908ba199bb8e231
Sergey Bronskiy <s.s.bronskiy@gmail.com>
2017-11-23 02:53:43 +0300

    data merged and selected, activities named

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,25 @@
 test <- read.table("test/X_test.txt", as.is = TRUE)
+testSubject <- read.table("test/subject_test.txt", as.is = TRUE)
+testActivity <- read.table("test/y_test.txt", as.is = TRUE)
+
 training <- read.table("train/X_train.txt", as.is = TRUE)
+trainSubject <- read.table("train/subject_train.txt", as.is = TRUE)
+trainActivity <- read.table("train/y_train.txt", as.is = TRUE)
+
 features <- read.table("features.txt", as.is = TRUE)
+activityLabels <- read.table("activity_labels.txt", as.is = TRUE)
+
 measurements <- rbind(test, training)
-rm("test")
-rm("training")
-features <- read.table("features.txt", as.is = TRUE)
+names(measurements) <- paste(features[[1]],"-", features[[2]], sep = "")
 
+subjects <- rbind(testSubject, trainSubject)
+activitys <- rbind(testActivity, trainActivity)
 
-teset1 <- strsplit(features[[2]], split = "\\-")
-te2 <- do.call(cbind.fill, c(lapply(teset1, data.frame, stringsAsFactors = FALSE), fill = NA))
-te2[] <- lapply(te2, as.character)
-te3 = as.data.frame(t(te2), stringsAsFactors = FALSE)
+measurements$subject <- subjects[[1]]
+measurements$activity <- activitys[[1]]
+
+tidyDataset <-select(measurements, contains("mean()"), contains("std()"), subject, activity)
+
+tidyDataset$activity <- as.factor(tidyDataset[["activity"]])
+tidyDataset$activity <- mapvalues(tidyDataset$activity, from = activityLabels[[1]], to = activityLabels[[2]] )
+
